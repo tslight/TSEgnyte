cccc1d0b2d6dea2fd399f1e8185bbabc7aa1a807
tslight <tobyslight@gmail.com>
2019-02-25 17:49:57 +0000

    better comment

--- Public/ConvertFrom-EgnyteGroups.ps1
+++ Public/ConvertFrom-EgnyteGroups.ps1
@@ -17,7 +17,8 @@ function ConvertFrom-EgnyteGroups {
 	$GroupObj | Add-Member -MemberType NoteProperty -Name "Permissions" -Value $Groups[$Name]
 	if ($Permissions.Groups.Name -Contains $Name) {
 	    Write-Host -Back Black -Fore Cyan "Already queried $Name group."
-	    $Members = ($Permissions.Groups | ? { $_.Name -eq $Name })[0].Members # only need first occurance otherwise this grows expontentially!
+	    # only need first element of members array, otherwise this grows expontentially and we run out of memory!
+	    $Members = ($Permissions.Groups | ? { $_.Name -eq $Name })[0].Members
 	} else {
 	    Write-Host -Back Black -Fore Magenta "Getting members of $Name group..."
 	    $Members = (Get-ADGroupMembersByName $Name -Quick).Name
