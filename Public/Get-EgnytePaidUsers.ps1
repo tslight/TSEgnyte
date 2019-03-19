f4dc61f7f97a5838e858fbfea1f0fc60314a7f9b
tslight <tobyslight@gmail.com>
2019-03-19 11:12:20 +0000

    add admin users

--- Public/Get-EgnytePaidUsers.ps1
+++ Public/Get-EgnytePaidUsers.ps1
@@ -1,11 +1,12 @@
 function Get-EgnytePaidUsers {
     [CmdletBinding(SupportsShouldProcess)]
     Param (
-	[object]$Session,
+	[object]$Session=(Get-EgnyteSession),
 	[Parameter(ValueFromPipeline)]
-	[object[]]$Users=(Get-EgnyteAllUsers -Session $Session)
+	[object[]]$Users=(Get-EgnyteUsers -Session $Session)
     )
 
-    $Users | Where-Object { $_.active -eq $True -And $_.userType -eq 'power' }
-
+    $Users | Where-Object {
+	($_.active -eq $True) -And ($_.userType -eq 'power' -Or $_.userType -eq 'admin')
+    }
 }
